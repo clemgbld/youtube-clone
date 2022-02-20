378a35b88c563af3df36c1e0d348d730a407e302
clemgbld <clementgombauld@hotmail.fr>
2022-02-20 16:52:25 +0100

    new key api

--- api/YoutubeApi/apiYoutube.ts
+++ api/YoutubeApi/apiYoutube.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const KEY = "AIzaSyCHCl7sIIMNc3VkIxZkztA0wHR7YpJ6BEU";
+export const KEY = "AIzaSyCYANcLxpylqUxT1XT9_5RSloLyxqSEwm4";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
