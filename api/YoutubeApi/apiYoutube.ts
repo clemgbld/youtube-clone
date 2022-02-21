885be103ed915ebd518e2f82418a6f6d389e8546
clemgbld <clementgombauld@hotmail.fr>
2022-02-21 07:43:03 +0100

    key changed

--- api/YoutubeApi/apiYoutube.ts
+++ api/YoutubeApi/apiYoutube.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const KEY = "AIzaSyCYANcLxpylqUxT1XT9_5RSloLyxqSEwm4";
+export const KEY = "AIzaSyDHHbfAAoH7GUEPw5IeinKs3hbZp6w5JuE";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
